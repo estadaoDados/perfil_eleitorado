b4abd64d69918fd3997e13d7e76bb0d877eec120
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2014-08-14 18:09:53 -0300

    plotando 12 gráficos

--- js/perfil-eleitorado.js
+++ js/perfil-eleitorado.js
@@ -2,15 +2,30 @@ var Main = (function() {
 
     var candidato ='eduardo campos',
         defaulCand = 'eduardo campos',
-        categorias = ["idade","renda_familiar","escolaridade","regiao","condicao_municipio","religiao","cor","interesse","vida_hoje","avaliacao_governo","desejo_mudanca","2turno_aecio"],
+        categoria = ["idade","renda_familiar","escolaridade","regiao","condicao_municipio","religiao","cor","interesse","vida_hoje","avaliacao_governo","desejo_mudanca","2turno_aecio"],
         possibilidades = ["eduardo campos","dilma roussseff", "aecio neves","pastor everaldo","outros","brancos e nulos","indecisos"],
         complete_data = null,
         meuGrafico = null,
-        svg = null;
+        lista_svgs = {
+            "idade": null,
+            "renda_familiar": null,
+            "escolaridade": null,
+            "regiao": null,
+            "condicao_municipio": null,
+            "religiao": null,
+            "cor": null,
+            "interesse": null,
+            "vida_hoje": null,
+            "avaliacao_governo": null,
+            "desejo_mudanca": null,
+            "2turno_aecio": null
+        };
 
     function inicializa() {
         //TODO: Criar os 12 svgs
-        svg = dimple.newSvg("#graf-idade", "100%", "80%");
+        for (var i=0 ; i < categoria.length ; i++) {
+            lista_svgs[categoria[i]] = dimple.newSvg("#graf-"+categoria[i], "100%", "80%");
+        }
 
         //desenha grafico
         d3.csv("dados/dados_perfil.csv", function (data) {
@@ -34,7 +49,9 @@ var Main = (function() {
                 }
             };
 
-            _cria_grafico();
+            var lista_candidatos = ["dilma"];
+
+            _cria_graficos(lista_candidatos);
         });
 
         window.onresize = function () {
@@ -46,31 +63,41 @@ var Main = (function() {
         };
     }
 
+    function _cria_graficos(lista_candidatos){
+        for (var i=0 ; i < categoria.length ; i++ ) {
+            var svg = lista_svgs[categoria[i]];
+            _cria_grafico(svg, lista_candidatos, categoria[i]);
+        }
+    }
+
     function _atualiza_background() {
         d3.select("#background_plot_area")
             .attr("width", dimple._parentWidth(svg.node()) - 225 )
             .attr("height", dimple._parentHeight(svg.node()) - 100)
     }
 
-    function _cria_grafico() {
-        data = dimple.filterData(window.complete_data, "candidato", ["total","dilma"])
-        data = dimple.filterData(data, "categoria", "idade")
+    function _cria_grafico(svg, lista_candidatos, categoria) {
+        if (lista_candidatos.indexOf("total") == -1) {
+            lista_candidatos.unshift("total");
+        }
+        data = dimple.filterData(window.complete_data, "candidato", lista_candidatos)
+        data = dimple.filterData(data, "categoria", categoria)
         var myChart = new dimple.chart(svg, data);
         myChart.setBounds(45,20,"85%","80%");
-        myChart.setMargins("60px","30px","165px","70px");
+        //myChart.setMargins("60px","30px","165px","70px");
         myChart.addMeasureAxis("y", "valor");
         var y = myChart.addCategoryAxis("x", ["dado","candidato"]);
         y.title = ""
         series = myChart.addSeries("candidato", dimple.plot.bar);
         series.barGap = 0.42;
         svg
-   	 	    .append("text").text("Idade")
+   	 	    .append("text").text(categoria)
    		    .attr("x","37%")
    		    .attr("y","20px")
            .style("font-family", "sans-serif")
            .style("font-size", "20px")
-           .style("color", "Black");			;	
-        
+           .style("color", "Black");			;
+
         myChart.draw();
     }
 
