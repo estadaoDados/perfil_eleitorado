97d1167fc671212d0cd33d9b059978ebf6b4b852
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2014-08-15 17:49:24 -0300

    Barra superior adicionada

--- js/perfil-eleitorado.js
+++ js/perfil-eleitorado.js
@@ -3,7 +3,7 @@ var Main = (function() {
     var candidato ='eduardo campos',
         defaulCand = 'eduardo campos',
         categoria = ["idade","renda_familiar","escolaridade","regiao","condicao_municipio","religiao","cor","interesse","vida_hoje","avaliacao_governo","desejo_mudanca","2turno_aecio"],
-        possibilidades = ["eduardo campos","dilma roussseff", "aecio neves","pastor everaldo","outros","brancos e nulos","indecisos"],
+        possibilidades = ["campos","dilma", "aecio","pastor","outros","brancos","indecisos"],
         complete_data = null,
         meuGrafico = null,
         lista_svgs = {
@@ -19,7 +19,8 @@ var Main = (function() {
             "avaliacao_governo": null,
             "desejo_mudanca": null,
             "2turno_aecio": null
-        };
+        },
+        lista_charts = {};
 
     function inicializa() {
         //TODO: Criar os 12 svgs
@@ -37,12 +38,12 @@ var Main = (function() {
 
             crossroads.addRoute('/candidato/{cand}', function(cand){
                 cand = decodeURI(cand);
-                candidato = possibilidades.indexOf(cand) > -1 ? cand : defaultFilters.pergunta;
+                candidato = possibilidades.indexOf(cand) > -1 ? cand : "campos";
             });
 
             crossroads.routed.add(function(request, data){
                 window.location.hash = "#candidato/" + encodeURI(candidato);
-                _atualiza_grafico();
+                _atualiza_graficos();
             });
 
             var a = $('.link-cand');
@@ -53,7 +54,7 @@ var Main = (function() {
                 }
             };
 
-            var lista_candidatos = ["dilma"];
+            var lista_candidatos = ["campos"];
 
             _cria_graficos(lista_candidatos);
         });
@@ -62,8 +63,11 @@ var Main = (function() {
             // As of 1.1.0 the second parameter here allows you to draw
             // without reprocessing data.  This saves a lot on performance
             // when you know the data won't have changed.
-            //chart.draw(0, true);
-            //_atualiza_background();
+            for (var i=0 ; i < categoria.length ; i++ ) {
+                var chart = lista_charts[categoria[i]];
+                chart.draw(0,true);
+                _atualiza_background(categoria[i]);
+            }
         };
     }
 
@@ -74,44 +78,41 @@ var Main = (function() {
         }
     }
 
-    function _atualiza_background() {
-        d3.select("#background_plot_area")
+    function _atualiza_background(categoria) {
+        var svg = d3.select("#graf-" + categoria + " svg");
+        d3.select("#graf-"+categoria)
             .attr("width", dimple._parentWidth(svg.node()) - 225 )
             .attr("height", dimple._parentHeight(svg.node()) - 100)
     }
 
-    function _cria_grafico(svg, lista_candidatos, categoria) {
-        if (lista_candidatos.indexOf("total") == -1) {
-            lista_candidatos.unshift("total");
-        }
-        data = dimple.filterData(window.complete_data, "candidato", "dilma")
-        data_total = dimple.filterData(window.complete_data, "candidato", "total")
-        data = dimple.filterData(data, "categoria", categoria)
-        data_total = dimple.filterData(data_total, "categoria", categoria)
-        var myChart = new dimple.chart(svg, data);
-        myChart.setBounds(45,30,"80%","65%");
-        //myChart.setMargins("10px","0px","10px","40px");
-        myChart.addMeasureAxis("y", "valor");
-        var y = myChart.addCategoryAxis("x", "dado");
-        y.title = ""
-        y.addOrderRule(["16 a 24","25 a 34","35 a 44","45 a 54","55 ou mais"])
-        y.addGroupOrderRule(["total","dilma"])
-        series = myChart.addSeries("candidato", dimple.plot.bar);
-        series.addOrderRule(["total","dilma"])
-        series.barGap = 0.3;
-        
-       var myChart2 = new dimple.chart(svg, data_total);
-       myChart2.setBounds(45,20,"85%","80%");
+    function _cria_grafico(svg, candidato, categoria) {
 
-        var y2 = myChart2.addCategoryAxis("x", "dado");
-        y2.addOrderRule(["16 a 24","25 a 34","35 a 44","45 a 54","55 ou mais"])
-        
-        y2.title = "";
-        //myChart.setMargins("60px","30px","165px","70px");
-        myChart2.addMeasureAxis("y", "valor");
-        series2 = myChart2.addSeries("candidato", dimple.plot.bar);
-        series2.stacked = false
-        
+        data_cand = dimple.filterData(window.complete_data, "candidato", "dilma");
+        data_cand = dimple.filterData(data_cand, "categoria", categoria);
+
+        data_total = dimple.filterData(window.complete_data, "candidato", "total");
+        data_total = dimple.filterData(data_total, "categoria", categoria);
+
+        var chart = new dimple.chart(svg);
+            chart.setBounds(25,30,"65%","75%");
+
+        var x = chart.addCategoryAxis("x","dado");
+            x.title= ""
+
+        var y = chart.addMeasureAxis("y", "valor");
+            y.title = "";
+            y.addOrderRule(["16 a 24","25 a 34","35 a 44","45 a 54","55 ou mais"]);
+
+        var serie_cand = chart.addSeries("candidato", dimple.plot.bar,[x, y]);
+            serie_cand.data = data_cand;
+            serie_cand.barGap = 0.2;
+
+        var serie_total = chart.addSeries("candidato",dimple.plot.bar,[x, y]);
+            serie_total.data = data_total;
+            serie_total.stacked = false;
+
+        chart = _configuraCores(chart);
+
         svg.append("text").text(categoria)
            .attr("x","37%")
            .attr("y","20px")
@@ -119,10 +120,11 @@ var Main = (function() {
            .style("font-size", "20px")
            .style("color", "Black");
 
-        myChart = _configuraCores(myChart)
-        myChart2 = _configuraCores(myChart2)
-        myChart.draw();
-        myChart2.draw();
+        chart.staggerDraw = true;
+        chart.ease = "bounce";
+        chart.draw(500);
+
+        lista_charts[categoria] = chart;
     }
 
     function _ordemLegenda(recorte) {
@@ -139,20 +141,25 @@ var Main = (function() {
         return grafico;
     }
 
-
     function _limpar_legendas() {
         $('*[class^="dimple-legend"]').remove()
     }
 
-    function _atualiza_grafico() {
+    function _atualiza_graficos() {
+        var data_cand = dimple.filterData(window.complete_data, "candidato", candidato);
+
+        for (var i=0 ; i < categoria.length ; i++ ) {
+            _atualiza_graf_categoria(data_cand, lista_charts[categoria[i]], categoria[i]);
+        }
     }
 
-    function atualiza_recorte(cat_recorte, recorte, texto){
-        _atualiza_grafico({cat_recorte: cat_recorte, recorte: recorte, texto_recorte: texto});
-    }
+    function _atualiza_graf_categoria(dados, chart, categoria){
+        var data_cand = dimple.filterData(dados, "categoria", categoria);
 
-    function atualiza_pergunta(pergunta, texto){
-        _atualiza_grafico({pergunta: pergunta, texto_pergunta: texto})
+        chart.series[0].data = data_cand;
+        chart.draw(500);
+
+        lista_charts[categoria] = chart;
     }
 
     return {
